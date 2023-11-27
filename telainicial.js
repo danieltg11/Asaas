e9864ec9d156e9ca691880966c47816bcb19d7e9
Legobytes <146657080+danieltg11@users.noreply.github.com>
2023-11-26 21:01:07 -0300

    Add files via upload

---
+++ telainicial.js
@@ -0,0 +1,38 @@
+function oua1(){
+    window.location.href = '#gas'
+}
+function oua2(){
+    window.location.href = 'http://127.0.0.1:5500/Legobytes/Tela%20de%20acesso%20ao%20sensor%20de%20g%C3%A1s/site.html'}
+function oua3(){
+    var btn3 = document.getElementById('btn3')
+    btn3.style.backgroundColor = '#028CFF'
+}
+function oua4(){
+    window.location.href = 'https://www.instagram.com/lego_bytes/'
+}
+function oua5(){
+    var btn5 = document.getElementById('btn5')
+    btn5.style.backgroundColor = '#028CFF'
+}
+function oua6(){
+    window.location.href = '#qmsomoss'
+}
+function qntdgas(){
+    var co2 = window.document.getElementById('co2').value
+    var h4 = document.getElementById('h4')
+
+    if (co2 =='Bom'){
+        ppm.innerHTML = 'cuzxin';
+       
+
+
+    }
+    else{
+        h4.innerHTML = 'Dados incorretos. Tente novamente'
+
+    }
+
+}
+function voltar(){
+    window.location.href = 'http://127.0.0.1:5500/Legobytes%20-%20Copia/Tela%20Inicial/telainicial.html'
+}
